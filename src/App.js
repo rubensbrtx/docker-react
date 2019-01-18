f1a771e4fa5aef031625fc7bde12f071dc6f359f
rubens brito <rubensbrtx@gmail.com>
2019-01-18 08:40:50 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            DevOps Project 2019
+          </p>
+	  <p>
+	    2021 Goals
+	  </p>
+	  <p>
+	   Monitoria carai !!!!
+	  </p>  
+         <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
